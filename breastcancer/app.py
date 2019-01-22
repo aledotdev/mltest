1b0d0da5035cd897c695f221b673db07945a2a4c
Ale Devalis <alejandrodevalis@gmail.com>
2019-01-22 17:00:04 -0300

    Refactor app init and settings

--- breastcancer/app.py
+++ breastcancer/app.py
@@ -3,41 +3,38 @@ import logging.config
 
 from flask import Flask, Blueprint
 
-from breastcancer import settings
 from breastcancer.model import load_model
 from breastcancer.api.common import api
 from breastcancer.api.predictor.endpoints import ns as breast_cancer_predictor_ns
 
 
-app = Flask(__name__)
 logging_conf_path = os.path.normpath(os.path.join(os.path.dirname(__file__), '../logging.conf'))
 logging.config.fileConfig(logging_conf_path)
 log = logging.getLogger(__name__)
 
 
-def configure_app(flask_app):
-    flask_app.config['SERVER_NAME'] = settings.FLASK_SERVER_NAME
-    flask_app.config['SWAGGER_UI_DOC_EXPANSION'] = settings.RESTPLUS_SWAGGER_UI_DOC_EXPANSION
-    flask_app.config['RESTPLUS_VALIDATE'] = settings.RESTPLUS_VALIDATE
-    flask_app.config['RESTPLUS_MASK_SWAGGER'] = settings.RESTPLUS_MASK_SWAGGER
-    flask_app.config['ERROR_404_HELP'] = settings.RESTPLUS_ERROR_404_HELP
-    load_model(settings.MODEL_FILE_PATH)
+def initialize_app(settings_class):
+    app = Flask(__name__)
+    app.config.from_object(settings_class)
+    log.info(">>>>> Setting up app config from {}".format(settings_class))
 
-
-def initialize_app(flask_app):
-    configure_app(flask_app)
+    # import ipdb; ipdb.set_trace()
+    load_model(app.config['MODEL_FILE_PATH'])
+    log.info(">>>>> Loaded Breast Cancer Model {}".format(app.config['MODEL_FILE_PATH']))
 
     blueprint = Blueprint('api', __name__, url_prefix='/api')
     api.init_app(blueprint)
     api.add_namespace(breast_cancer_predictor_ns)
-    flask_app.register_blueprint(blueprint)
+    app.register_blueprint(blueprint)
+
+    return app
 
 
 def main():
-    initialize_app(app)
-    log.info(('>>>>> Starting development server at http://{}/api/ <<<<<'
+    app = initialize_app(os.getenv('BC_SETTING_CLASS', 'breastcancer.settings.DevSettings'))
+    log.info(('>>>>> Starting development server at http://{}/api/'
               .format(app.config['SERVER_NAME'])))
-    app.run(debug=settings.FLASK_DEBUG)
+    app.run()
 
 
 if __name__ == "__main__":
--- breastcancer/settings.py
+++ breastcancer/settings.py
@@ -1,14 +1,32 @@
 import os
 
-# Flask settings
-FLASK_SERVER_NAME = 'localhost:{}'.format(int(os.getenv('BC_SERVER_PORT', 5000)))
-FLASK_DEBUG = bool(os.getenv('BC_DEBUG', True))  # Do not use debug mode in production
 
-# Flask-Restplus settings
-RESTPLUS_SWAGGER_UI_DOC_EXPANSION = 'list'
-RESTPLUS_VALIDATE = True
-RESTPLUS_MASK_SWAGGER = False
-RESTPLUS_ERROR_404_HELP = False
+class BaseSettings(object):
+    # Flask settings
+    FLASK_SERVER_NAME = 'localhost:{}'.format(int(os.getenv('BC_SERVER_PORT', 5000)))
+    FLASK_DEBUG = bool(os.getenv('BC_DEBUG', False))  # Do not use debug mode in production
+    SECRET_KEY = os.getenv('BC_SECRET_KEY')
 
-# MODEL
-MODEL_FILE_PATH = os.getenv('MODEL_FILE_PATH')
+    # Flask-Restplus settings
+    RESTPLUS_SWAGGER_UI_DOC_EXPANSION = 'list'
+    RESTPLUS_VALIDATE = True
+    RESTPLUS_MASK_SWAGGER = False
+    RESTPLUS_ERROR_404_HELP = False
+
+    # MODEL
+    MODEL_FILE_PATH = os.getenv('BC_MODEL_FILE_PATH')
+
+
+class ProdSettings(BaseSettings):
+    pass
+
+
+class DevSettings(BaseSettings):
+    DEBUG = bool(os.getenv('BC_DEBUG', True))  # Do not use debug mode in production
+    SECRET_KEY = "DEV-SECRET-123456789"
+
+
+class TestSettings(BaseSettings):
+    DEBUG = False
+    TESTING = True
+    SECRET_KEY = "TEST-SECRET-123456789"
